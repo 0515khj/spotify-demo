7c194085c0f5e16f0a63acd694742a1c512217d0
0515khj <kh32100@naver.com>
2026-01-03 23:42:50 +0900

    수정

--- src/apis/searchApi.ts
+++ src/apis/searchApi.ts
@@ -1,17 +1,17 @@
-import axios from "axios";
-import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
+// import axios from "axios";
+// import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
 
-const searchItemsByKeyword =async(token,params)=>{
-    try {
-        const response = await axios.get(`${SPOTIFY_BASE_URL}/search?${}`,{
-            headers:{
-                Authorization:`Bearer ${token}`,
-                "Content-Type": "application/json",
-            }
-        })
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw new Error('fail to search by keyword')
-    }
-}
+// const searchItemsByKeyword =async(token,params)=>{
+//     try {
+//         const response = await axios.get(`${SPOTIFY_BASE_URL}/search?${}`,{
+//             headers:{
+//                 Authorization:`Bearer ${token}`,
+//                 "Content-Type": "application/json",
+//             }
+//         })
+//         return response.data;
+//     } catch (error) {
+//         console.log(error);
+//         throw new Error('fail to search by keyword')
+//     }
+// }
